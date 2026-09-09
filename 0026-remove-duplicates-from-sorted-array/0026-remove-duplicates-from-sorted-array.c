8cc61e09133524d8a9721d6ae9c2505f80988bde
Mohammed Thaseen <167888242+MDThaseen@users.noreply.github.com>
2026-09-09 18:59:07 +0530

    Time: 0 ms (100%), Space: 13 MB (40.16%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,10 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int l = 1;
+    for (int r = 1; r < numsSize; r++) {
+        if (nums[r] != nums[r - 1]) {
+            nums[l] = nums[r];
+            l++;
+        }
+    }
+    return l;
+}
